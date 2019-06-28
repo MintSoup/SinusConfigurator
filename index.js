fde3cbd82fab83c4b7b52df21a16312cac8889cc
MintSoup <aregak2005@gmail.com>
2019-06-28 16:41:04 +0400

    added /servers/

--- index.js
+++ index.js
@@ -16,12 +16,11 @@ app.use(express.urlencoded());
 
 
 app.get('/settings/:id', function (req, res) {
-    sql.run(`select * from servers where id=${req.params.id}`, function (data) {
-        try{
+    sql.run(`select * from servers where id='${req.params.id}'`, function (data) {
+        try {
             res.setHeader('Content-Type', 'application/json');
             res.send((data[0]["settings"]))
-        }
-        catch(err){
+        } catch (err) {
             res.send("invalid id")
         }
     })
@@ -30,17 +29,46 @@ app.get('/settings/:id', function (req, res) {
 app.post("/set/:id", function (req, res) {
 
     if (!req.body.password)
-        res.send("please send password sha3-256").end()
+        res.send("please send password").end()
     else {
         if (checkjson(req.body.settings)) {
-            
+
             set(req.params.id, req.body.password, req.body.settings, function (msg) {
                 res.send(msg).end()
             })
-        }
-        else{
+        } else {
             res.send("please send a valid JSON settings string").end()
         }
+    }
+})
+
+app.get("/servers/", function (req, res) {
+    if (req.query.password) {
+        var hash = new SHA3(256);
+        hash.update(req.query.password)
+        if (hash.digest("hex") == "5ea27761bbc70b4583c99f6beceec3098c53a80c143bc0623e3a4bb8ddc73108") {
+            if (req.query.servers) {
+                var servers = JSON.parse(req.query.servers)
+                sql.run("select * from servers", function (data) {
+                    data.forEach(server => {
+                        if (!servers.includes(server["id"])) {
+                            console.log(server["id"])
+                            sql.run(`delete from servers where id='${server["id"]}'`, function (data) {
+                                
+                            })
+                        }
+                    });
+
+                    res.send("ok")
+                })
+            } else {
+                res.send("L")
+            }
+        } else {
+            res.send("invalid password")
+        }
+    } else {
+        res.send("pls send password")
     }
 })
 
@@ -48,19 +76,19 @@ app.post("/set/:id", function (req, res) {
 
 function set(id, password, json, callback) {
 
-    sql.run(`select * from servers where id=${id}`, function (data) {
+    sql.run(`select * from servers where id='${id}'`, function (data) {
 
         var hash = new SHA3(256);
         hash.update(password)
         var hex = hash.digest("hex");
         if (data.length == 0) {
-            sql.run(`insert into servers values (${id}, '${hex}', '${json}')`, function (data) {
+            sql.run(`insert into servers values ('${id}', '${hex}', '${json}')`, function (data) {
                 callback("created new field")
             })
         } else {
             if (hex == data[0]["password"]) {
-                
-                sql.run(`update servers set settings='${json}' where id=${id}`, function () {
+
+                sql.run(`update servers set settings='${json}' where id='${id}'`, function () {
                     callback("updated field")
                 })
             } else
